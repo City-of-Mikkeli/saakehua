1afafcb558e04c9a39781eba035533f3797e8c7c
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-06-21 20:22:42 +0300

    ** Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var express = require('express');
+var pug = require('pug');
+
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
