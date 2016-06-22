6dfa5d73920c34052e4226e8610bda66198a7580
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-06-22 12:00:45 +0300

    ** Added client side template generating and now retweets are not posted anymore

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ var pug = require('pug');
 var conf = require('./config');
 var ig = require('./scrapers/instaScraper');
 var Twitter = require('twitter');
+var fs = require('fs');
+
+var clientTemplates = fs.readdirSync(__dirname + '/views/client');
+var compiledClientTemplates = [];
+for (var i = 0; i < clientTemplates.length; i++) {
+  var templateName = clientTemplates[i].replace('.pug', '');
+  templateName = 'render'+templateName[0].toUpperCase() + templateName.substring(1);
+  compiledClientTemplates.push(pug.compileFileClient(
+    __dirname + '/views/client/' + clientTemplates[i],
+    { name: templateName}
+  ));
+}
+fs.writeFileSync(__dirname + '/public/script/templates.js', compiledClientTemplates.join(''));
 
 var client = new Twitter({
   consumer_key: conf.twitter.consumer_key,
@@ -31,14 +44,17 @@ io.on('connection', function (socket) {
 
 client.stream('statuses/filter', {track: '#food'},  function(stream) {
   stream.on('data', function(tweet) {
-    var data = {
-      _id: tweet.id,
-      text: tweet.text,
-      img: tweet.entities.media ? tweet.entities.media[0].media_url_https : null,
-      tags: tweet.entities.hashtags.map(function(tag){ return '#'+tag.text; }),
-      date: new Date(tweet.created_at)
+    if(!tweet.retweeted_status) {
+      var data = {
+        _id: tweet.id,
+        text: tweet.text,
+        img: tweet.entities.media ? tweet.entities.media[0].media_url_https : null,
+        tags: tweet.entities.hashtags.map(function(tag){ return '#'+tag.text; }),
+        date: new Date(tweet.created_at),
+        icon: 'fa fa-twitter'
+      };
+      io.emit('post:received', data);
     }
-    io.emit('post:received', data);
   });
 
   stream.on('error', function(error) {
