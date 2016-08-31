2a18ca78181b4be042323dc62f169a44fc93b22e
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-08-31 13:45:22 +0300

    ** Added loading more content when scrolled to bottom

--- public/script/main.js
+++ public/script/main.js
@@ -2,6 +2,7 @@
   'use strict';
   
   var socket = io();
+  var contentLoading = false;
   
   $(document).ready(function(){
     $('.grid').masonry({
@@ -9,6 +10,27 @@
       columnWidth: '.grid-sizer',
       percentPosition: true
     });
+  });
+  
+  $(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+     if(!contentLoading){
+      socket.emit('load:more');
+      contentLoading = true;
+     }
+
+   }
+  });
+  
+  socket.on('more:loaded', function(data){
+    for(var i = 0; i < data.length;i++) {
+      var item = $(renderMediaItem(data[i]));
+      $('.grid').append(item).masonry( 'appended', item );
+      item.imagesLoaded(function(){
+        $('.grid').masonry('layout');
+      });    
+    }
+    contentLoading = false;
   });
   
   socket.on('post:received', function(data){
