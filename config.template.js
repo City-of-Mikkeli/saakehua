f792041a645e07596b7fa1b34bddbec4e67b249b
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-09-03 13:42:31 +0300

    Updated config template

--- config.template.js
+++ config.template.js
@@ -1,12 +1,15 @@
 module.exports = {
   port: 3000,
+  supportedMimetypes : [
+    'image/png',
+    'image/jpeg',
+    'image/gif'
+  ],
   twitter: {
     consumer_key: '',
     consumer_secret: '',
-    access_token_key: '-',
+    access_token_key: '',
     access_token_secret: ''
   },
-  youtube: {
-    
-  }
+  tags: []
 }
